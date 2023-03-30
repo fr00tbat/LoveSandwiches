f34a7e0bc1cb45de19a0202b087125152263e811
fr00tbat <fruitbat@fruitbat.ie>
2023-03-30 10:49:08 +0000

    Calculate stock numbers and add to worksheet

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
 from pprint import pprint
+import math
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -129,6 +130,23 @@ def get_last_5_entries_sales():
     return columns   
 
 
+
+def calculate_stock_data(data):
+    """
+    Calculate average stock for each item type, adding 10%
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column)/len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
+
+
 def main():
     """
     Run all programs functions
@@ -138,9 +156,13 @@ def main():
     update_worksheet(sales_data, "sales")
     calcd_surplus_data = calculate_surplus_data(sales_data)                                                                        
     update_worksheet(calcd_surplus_data, "surplus")
-    
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    print(stock_data)
+    update_worksheet(stock_data, "stock")    
     
 
 print("Welcome to Love Sandwiches Data Automation")
-#main()
-sales_columns = get_last_5_entries_sales()
+main()
+
+
